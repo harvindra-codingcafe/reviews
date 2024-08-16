108c9e9364baeddd6c4ab52c88cb6e25112e78d9
Harvindra Singh <harvindra.singh@codingcafe.website>
2024-08-16 19:17:20 +0530

    Slick slider integrated

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
 import scene1 from "./assets/images/scene1-color.jpg";
 import scene2 from "./assets/images/scene1.jpg";
 import scene3 from "./assets/images/scene2-color.jpg";
-import scene4 from "./assets/images/scene2.jpg";
-import scene5 from "./assets/images/scene3-color.jpg";
-import scene6 from "./assets/images/scene2.jpg";
+
+import scene7 from "./assets/images/1.png";
+import scene8 from "./assets/images/2.png";
+import scene9 from "./assets/images/3.png";
+import scene10 from "./assets/images/4.png";
+import scene11 from "./assets/images/5.png";
+import scene12 from "./assets/images/6.png";
+import scene13 from "./assets/images/7.png";
+import scene14 from "./assets/images/8.png";
+import scene15 from "./assets/images/9.png";
 
 const data = [
   {
@@ -13,30 +20,93 @@ const data = [
     comments: [
       {
         id: 1,
-        name: "Monica",
-        time: "1h ago",
-        content:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        name: "Lucy",
+        time: "May 5",
+        content: "Awesome capture!",
       },
       {
         id: 2,
-        name: "Larry G",
-        time: "2 days ago",
-        content: "Awesome!",
+        name: "Tommy",
+        time: "1 day ago",
+        content: "Integer posuere",
       },
       {
         id: 3,
-        name: "Johny",
-        time: "May 30",
-        content:
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+        name: "John",
+        time: "2h ago",
+        content: "Nice and cool!",
       },
       {
         id: 4,
-        name: "Honey",
+        name: "Anna",
+        time: "May 10",
+        content: "Wonderful pic!",
+      },
+      {
+        id: 5,
+        name: "David",
+        time: "8h ago",
+        content: "Great picture!",
+      },
+      {
+        id: 6,
+        name: "Emily",
+        time: "May 12",
+        content: "Looks amazing!",
+      },
+      {
+        id: 7,
+        name: "Sara",
+        time: "2h ago",
+        content: "Beautiful shot",
+      },
+      {
+        id: 8,
+        name: "Tom",
+        time: "1 day ago",
+        content: "Awesome colors!",
+      },
+      {
+        id: 9,
+        name: "Jessica",
+        time: "3h ago",
+        content: "Very impressive",
+      },
+      {
+        id: 10,
+        name: "Jerry",
+        time: "2 days ago",
+        content: "Love this one!",
+      },
+      {
+        id: 11,
+        name: "Frank",
+        time: "5h ago",
+        content: "Superb capture!",
+      },
+      {
+        id: 12,
+        name: "Nancy",
+        time: "2 days ago",
+        content: "Nice colors!",
+      },
+      {
+        id: 13,
+        name: "Monica",
+        time: "1h ago",
+        content: "Pretty amazing",
+      },
+      {
+        id: 14,
+        name: "Larry",
         time: "May 15",
-        content:
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,",
+        content: "Great details!",
+      },
+      {
+        id: 15,
+        name: "Honey",
+        time: "May 30",
+        content: "Perfect shot!",
       },
     ],
   },
@@ -68,80 +138,463 @@ const data = [
     comments: [
       {
         id: 1,
-        name: "Monica",
-        time: "1h ago",
-        content:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        name: "Lucy",
+        time: "May 5",
+        content: "Awesome capture!",
+      },
+      {
+        id: 2,
+        name: "Tommy",
+        time: "1 day ago",
+        content: "Integer posuere",
+      },
+      {
+        id: 3,
+        name: "John",
+        time: "2h ago",
+        content: "Nice and cool!",
       },
       {
         id: 4,
-        name: "Honey",
+        name: "Anna",
+        time: "May 10",
+        content: "Wonderful pic!",
+      },
+      {
+        id: 5,
+        name: "David",
+        time: "8h ago",
+        content: "Great picture!",
+      },
+      {
+        id: 6,
+        name: "Emily",
+        time: "May 12",
+        content: "Looks amazing!",
+      },
+      {
+        id: 7,
+        name: "Sara",
+        time: "2h ago",
+        content: "Beautiful shot",
+      },
+      {
+        id: 8,
+        name: "Tom",
+        time: "1 day ago",
+        content: "Awesome colors!",
+      },
+      {
+        id: 9,
+        name: "Jessica",
+        time: "3h ago",
+        content: "Very impressive",
+      },
+      {
+        id: 10,
+        name: "Jerry",
+        time: "2 days ago",
+        content: "Love this one!",
+      },
+      {
+        id: 11,
+        name: "Frank",
+        time: "5h ago",
+        content: "Superb capture!",
+      },
+      {
+        id: 12,
+        name: "Nancy",
+        time: "2 days ago",
+        content: "Nice colors!",
+      },
+      {
+        id: 13,
+        name: "Monica",
+        time: "1h ago",
+        content: "Pretty amazing",
+      },
+      {
+        id: 14,
+        name: "Larry",
         time: "May 15",
-        content:
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,",
+        content: "Great details!",
+      },
+      {
+        id: 15,
+        name: "Honey",
+        time: "May 30",
+        content: "Perfect shot!",
       },
     ],
   },
   {
     id: 4,
     name: "Lilly",
-    image: scene4,
-    comments: [
-      {
-        id: 1,
-        name: "Monica",
-        time: "1h ago",
-        content:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      },
-      {
-        id: 4,
-        name: "Honey",
-        time: "May 15",
-        content:
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,",
-      },
-    ],
+    image: scene7,
+    comments: [],
   },
   {
     id: 5,
     name: "Dania",
-    image: scene5,
+    image: scene8,
     comments: [
       {
         id: 1,
-        name: "Monica",
-        time: "1h ago",
-        content:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        name: "Lucy",
+        time: "May 5",
+        content: "Awesome capture!",
+      },
+      {
+        id: 2,
+        name: "Tommy",
+        time: "1 day ago",
+        content: "Integer posuere",
+      },
+      {
+        id: 3,
+        name: "John",
+        time: "2h ago",
+        content: "Nice and cool!",
       },
       {
         id: 4,
-        name: "Honey",
+        name: "Anna",
+        time: "May 10",
+        content: "Wonderful pic!",
+      },
+      {
+        id: 5,
+        name: "David",
+        time: "8h ago",
+        content: "Great picture!",
+      },
+      {
+        id: 6,
+        name: "Emily",
+        time: "May 12",
+        content: "Looks amazing!",
+      },
+      {
+        id: 7,
+        name: "Sara",
+        time: "2h ago",
+        content: "Beautiful shot",
+      },
+      {
+        id: 8,
+        name: "Tom",
+        time: "1 day ago",
+        content: "Awesome colors!",
+      },
+      {
+        id: 9,
+        name: "Jessica",
+        time: "3h ago",
+        content: "Very impressive",
+      },
+      {
+        id: 10,
+        name: "Jerry",
+        time: "2 days ago",
+        content: "Love this one!",
+      },
+      {
+        id: 11,
+        name: "Frank",
+        time: "5h ago",
+        content: "Superb capture!",
+      },
+      {
+        id: 12,
+        name: "Nancy",
+        time: "2 days ago",
+        content: "Nice colors!",
+      },
+      {
+        id: 13,
+        name: "Monica",
+        time: "1h ago",
+        content: "Pretty amazing",
+      },
+      {
+        id: 14,
+        name: "Larry",
         time: "May 15",
-        content:
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,",
+        content: "Great details!",
+      },
+      {
+        id: 15,
+        name: "Honey",
+        time: "May 30",
+        content: "Perfect shot!",
       },
     ],
   },
   {
     id: 6,
     name: "Honey",
-    image: scene6,
+    image: scene9,
+    comments: [],
+  },
+  // New objects added below
+  {
+    id: 7,
+    name: "Tom",
+    image: scene10,
     comments: [
       {
         id: 1,
-        name: "Monica",
-        time: "1h ago",
+        name: "Jessica",
+        time: "3h ago",
         content:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "Nulla facilisi. Cras justo odio, dapibus ac facilisis in, egestas eget quam.",
+      },
+      {
+        id: 2,
+        name: "Jerry",
+        time: "2 days ago",
+        content: "Great shot!",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Alice",
+    image: scene11,
+    comments: [
+      {
+        id: 1,
+        name: "Frank",
+        time: "5h ago",
+        content:
+          "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+      },
+      {
+        id: 2,
+        name: "Nancy",
+        time: "2 days ago",
+        content: "Nice one!",
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "Bob",
+    image: scene12,
+    comments: [
+      {
+        id: 1,
+        name: "Lucy",
+        time: "May 5",
+        content: "Awesome capture!",
+      },
+      {
+        id: 2,
+        name: "Tommy",
+        time: "1 day ago",
+        content: "Integer posuere",
+      },
+      {
+        id: 3,
+        name: "John",
+        time: "2h ago",
+        content: "Nice and cool!",
       },
       {
         id: 4,
-        name: "Honey",
+        name: "Anna",
+        time: "May 10",
+        content: "Wonderful pic!",
+      },
+      {
+        id: 5,
+        name: "David",
+        time: "8h ago",
+        content: "Great picture!",
+      },
+      {
+        id: 6,
+        name: "Emily",
+        time: "May 12",
+        content: "Looks amazing!",
+      },
+      {
+        id: 7,
+        name: "Sara",
+        time: "2h ago",
+        content: "Beautiful shot",
+      },
+      {
+        id: 8,
+        name: "Tom",
+        time: "1 day ago",
+        content: "Awesome colors!",
+      },
+      {
+        id: 9,
+        name: "Jessica",
+        time: "3h ago",
+        content: "Very impressive",
+      },
+      {
+        id: 10,
+        name: "Jerry",
+        time: "2 days ago",
+        content: "Love this one!",
+      },
+      {
+        id: 11,
+        name: "Frank",
+        time: "5h ago",
+        content: "Superb capture!",
+      },
+      {
+        id: 12,
+        name: "Nancy",
+        time: "2 days ago",
+        content: "Nice colors!",
+      },
+      {
+        id: 13,
+        name: "Monica",
+        time: "1h ago",
+        content: "Pretty amazing",
+      },
+      {
+        id: 14,
+        name: "Larry",
         time: "May 15",
+        content: "Great details!",
+      },
+      {
+        id: 15,
+        name: "Honey",
+        time: "May 30",
+        content: "Perfect shot!",
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: "Chris",
+    image: scene13,
+    comments: [],
+  },
+  {
+    id: 11,
+    name: "Eve",
+    image: scene14,
+    comments: [
+      {
+        id: 1,
+        name: "John",
+        time: "6h ago",
         content:
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,",
+          "Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum.",
+      },
+      {
+        id: 2,
+        name: "Anna",
+        time: "May 10",
+        content: "Beautiful!",
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: "Jack",
+    image: scene15,
+    comments: [
+      {
+        id: 1,
+        name: "Lucy",
+        time: "May 5",
+        content: "Awesome capture!",
+      },
+      {
+        id: 2,
+        name: "Tommy",
+        time: "1 day ago",
+        content: "Integer posuere",
+      },
+      {
+        id: 3,
+        name: "John",
+        time: "2h ago",
+        content: "Nice and cool!",
+      },
+      {
+        id: 4,
+        name: "Anna",
+        time: "May 10",
+        content: "Wonderful pic!",
+      },
+      {
+        id: 5,
+        name: "David",
+        time: "8h ago",
+        content: "Great picture!",
+      },
+      {
+        id: 6,
+        name: "Emily",
+        time: "May 12",
+        content: "Looks amazing!",
+      },
+      {
+        id: 7,
+        name: "Sara",
+        time: "2h ago",
+        content: "Beautiful shot",
+      },
+      {
+        id: 8,
+        name: "Tom",
+        time: "1 day ago",
+        content: "Awesome colors!",
+      },
+      {
+        id: 9,
+        name: "Jessica",
+        time: "3h ago",
+        content: "Very impressive",
+      },
+      {
+        id: 10,
+        name: "Jerry",
+        time: "2 days ago",
+        content: "Love this one!",
+      },
+      {
+        id: 11,
+        name: "Frank",
+        time: "5h ago",
+        content: "Superb capture!",
+      },
+      {
+        id: 12,
+        name: "Nancy",
+        time: "2 days ago",
+        content: "Nice colors!",
+      },
+      {
+        id: 13,
+        name: "Monica",
+        time: "1h ago",
+        content: "Pretty amazing",
+      },
+      {
+        id: 14,
+        name: "Larry",
+        time: "May 15",
+        content: "Great details!",
+      },
+      {
+        id: 15,
+        name: "Honey",
+        time: "May 30",
+        content: "Perfect shot!",
       },
     ],
   },
--- src/reviews/index.jsx
+++ src/reviews/index.jsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Comment from "./Comment";
 import CommentForm from "./CommentForm";
+import Slider from "react-slick";
 
 const CommentSection = () => {
   const [data, setData] = useState([]);
   const [selectedData, setSelectedData] = useState(null);
+
+  // Refs for the sliders
+  const topSliderRef = useRef(null);
+  const sideSliderRef = useRef(null);
 
   useEffect(() => {
     const storedData = localStorage.getItem("currentData");
@@ -14,65 +19,88 @@ const CommentSection = () => {
   }, []);
 
   const handleCommentAdded = (updatedComments) => {
-    // Update selectedData with new comments
     const updatedSelectedData = { ...selectedData, comments: updatedComments };
     setSelectedData(updatedSelectedData);
-
-    // Update the main data state
     const updatedData = data.map((item) =>
       item.id === updatedSelectedData.id ? updatedSelectedData : item
     );
     setData(updatedData);
   };
 
+  const handleSlideChange = (index) => {
+    // Synchronize both sliders when one changes
+    topSliderRef.current.slickGoTo(index);
+    sideSliderRef.current.slickGoTo(index);
+  };
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    centerMode: false,
+    beforeChange: (current, next) => handleSlideChange(next), // Track slide changes
+  };
+
+  var settings2 = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    centerMode: false,
+    vertical: true,
+    beforeChange: (current, next) => handleSlideChange(next), // Track slide changes
+  };
+
   return (
     <div className="container">
-      <div className="row m-0 top_bar">
-        {data?.map((item) => (
-          <div className="top_bar-single text-center col-2 p-0" key={item.id}>
-            <div className="bg-dark py-2">
-              <span
-                className={`w-100 d-block py-1 ${
-                  selectedData?.id === item.id ? "selected" : ""
-                }`}
-                onClick={() => setSelectedData(item)}
-                style={{
-                  borderTop: "2px dashed #fff",
-                  borderBottom: "2px dashed #fff",
-                }}
-              >
-                <span className="d-block bg-success">
-                  <img src={item.image} className="mw-100" alt={item.name} />
-                </span>
-              </span>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div style={{ height: "calc(100vh - 68px)" }}>
-        <div className="row h-100">
-          <div className="col-2 h-100">
-            <div className="left_bar h-100">
-              {data?.map((item) => (
-                <div
-                  className={`left_bar-single ${
+      <div className="m-0 top_bar">
+        <Slider {...settings} ref={topSliderRef}>
+          {data?.map((item) => (
+            <div className="top_bar-single text-center p-0" key={item.id}>
+              <div className="bg-dark py-2">
+                <span
+                  className={`w-100 d-block py-1 ${
                     selectedData?.id === item.id ? "selected" : ""
                   }`}
-                  key={item.id}
+                  onClick={() => setSelectedData(item)}
+                  style={{
+                    borderTop: "2px dashed #fff",
+                    borderBottom: "2px dashed #fff",
+                  }}
                 >
-                  <img
-                    onClick={() => setSelectedData(item)}
-                    src={item.image}
-                    className="w-100"
-                    alt={item.name}
-                  />
+                  <span className="d-block bg-success">
+                    <img src={item.image} className="mw-100" alt={item.name} />
+                  </span>
+                </span>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <div>
+        <div className="row h-100 mt-4 pt-2">
+          <div className="col-2 left_bar h-100">
+            <Slider {...settings2} ref={sideSliderRef}>
+              {data?.map((item) => (
+                <div key={item.id}>
+                  <div
+                    className={`left_bar-single ${
+                      selectedData?.id === item.id ? "selected" : ""
+                    }`}
+                  >
+                    <img
+                      onClick={() => setSelectedData(item)}
+                      src={item.image}
+                      className="w-100"
+                      alt={item.name}
+                    />
+                  </div>
                 </div>
               ))}
-            </div>
+            </Slider>
           </div>
           <div className="col-10">
             {selectedData ? (
-              <div className="comments mt-3">
+              <div className="comments">
                 <div className="card bg-light">
                   <div className="card-body">
                     <div className="all_comments">
